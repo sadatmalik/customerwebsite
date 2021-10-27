8cdbfe5ed70ad40240098780496d601cc2d546d2
sadat.malik <sadat.malik@me.com>
2021-10-27 19:14:50 +0100

    Created endpoint and template to display table of Car entities. Closes #8

--- src/main/java/com/sadatmalik/customerwebsite/controllers/CarController.java
+++ src/main/java/com/sadatmalik/customerwebsite/controllers/CarController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,7 +24,7 @@ public class CarController {
         return "new-car";
     }
 
-    @GetMapping("save-car")
+    @PostMapping("/save-car")
     public String save(@ModelAttribute("car") Car car, Model model) {
         try {
             carService.saveCar(car);
@@ -30,6 +33,13 @@ public class CarController {
             return "error";
         }
         return "redirect:/cars";
+    }
+
+    @GetMapping("/cars")
+    public String showCars(Model model) {
+        List<Car> cars = carService.getCars();
+        model.addAttribute("cars", cars);
+        return "car-index";
     }
 
 }
