01e27efbc9982f5a28349908ddafcf6e6936f522
sadat.malik <sadat.malik@me.com>
2021-10-27 14:14:07 +0100

    Create a new Car entity. Closes #4

---
+++ src/main/java/com/sadatmalik/customerwebsite/model/Car.java
@@ -0,0 +1,32 @@
+package com.sadatmalik.customerwebsite.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cars")
+@Builder
+@Getter
+@Setter
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String make;
+    private String model;
+
+    public void validate() throws IllegalStateException {
+        if (make == null || make.equals("")) {
+            throw new IllegalStateException("You must enter a make");
+        }
+
+        if (model == null || model.equals("")) {
+            throw new IllegalStateException("You must enter a model");
+        }
+    }
+
+}
