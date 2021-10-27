4da06d7fe9f9d3dd367418776e45f89496a5570b
sadat.malik <sadat.malik@me.com>
2021-10-27 14:48:44 +0100

    Creating an endpoint and template for creation of Cars. Closes #7

--- src/main/java/com/sadatmalik/customerwebsite/controllers/CarController.java
+++ src/main/java/com/sadatmalik/customerwebsite/controllers/CarController.java
@@ -1,10 +1,35 @@
 package com.sadatmalik.customerwebsite.controllers;
 
+import com.sadatmalik.customerwebsite.model.Car;
+import com.sadatmalik.customerwebsite.services.CarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @RequiredArgsConstructor
 @Controller
 public class CarController {
+
+    private final CarService carService;
+
+    @GetMapping("/new-car")
+    public String showNewCarPage(Model model) {
+        Car car = new Car();
+        model.addAttribute("car", car);
+        return "new-car";
+    }
+
+    @GetMapping("save-car")
+    public String save(@ModelAttribute("car") Car car, Model model) {
+        try {
+            carService.saveCar(car);
+        } catch (IllegalStateException e) {
+            model.addAttribute("error", e.getMessage());
+            return "error";
+        }
+        return "redirect:/cars";
+    }
 
 }
