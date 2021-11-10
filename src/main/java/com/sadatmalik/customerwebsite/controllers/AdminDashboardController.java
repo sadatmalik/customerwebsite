7dbddeb8923d9962de2c87176678c028aa97cbbc
sadat.malik <sadat.malik@me.com>
2021-11-10 14:04:49 +0000

    Implementing admin-dashboard template and target /endpoints; with supporting model objects in AdminDashboardController. Closes #24

--- src/main/java/com/sadatmalik/customerwebsite/controllers/AdminDashboardController.java
+++ src/main/java/com/sadatmalik/customerwebsite/controllers/AdminDashboardController.java
@@ -1,15 +1,33 @@
 package com.sadatmalik.customerwebsite.controllers;
 
+import com.sadatmalik.customerwebsite.model.Car;
+import com.sadatmalik.customerwebsite.model.Customer;
+import com.sadatmalik.customerwebsite.services.CarService;
+import com.sadatmalik.customerwebsite.services.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class AdminDashboardController {
 
+    @Autowired
+    private final CustomerService customerService;
+
+    @Autowired
+    private final CarService carService;
+
     @GetMapping("/admin-dashboard")
-    public String showAdminDashboard() {
+    public String showAdminDashboard(Model model) {
+        List<Customer> customers = customerService.getCustomers();
+        model.addAttribute("customerList", customers);
+        List<Car> cars = carService.getCars();
+        model.addAttribute("cars", cars);
         return "admin-dashboard";
     }
 }
