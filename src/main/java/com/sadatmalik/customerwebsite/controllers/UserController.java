4454f23485aa395a0a1e1ad91f1b42e5e5ca7f55
sadat.malik <sadat.malik@me.com>
2021-11-10 16:30:40 +0000

    Redirect after saving user - to admin dash if ADMIN; user dash if USER or else to index

--- src/main/java/com/sadatmalik/customerwebsite/controllers/UserController.java
+++ src/main/java/com/sadatmalik/customerwebsite/controllers/UserController.java
@@ -1,16 +1,21 @@
 package com.sadatmalik.customerwebsite.controllers;
 
+import com.sadatmalik.customerwebsite.model.Authority;
 import com.sadatmalik.customerwebsite.model.Customer;
 import com.sadatmalik.customerwebsite.model.User;
 import com.sadatmalik.customerwebsite.services.CustomerService;
 import com.sadatmalik.customerwebsite.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -31,12 +36,37 @@ public class UserController {
     }
 
     @PostMapping("/save-user")
-    public String registerNewUser(@ModelAttribute("user") User user, Model model) {
-        Customer savedCustomer = customerService.saveCustomer(user.getCustomer());
-        user.setCustomer(savedCustomer);
-        User savedUser = userService.saveUser(user);
+    public String registerNewUser(
+            @ModelAttribute("user") User user,
+            @CurrentSecurityContext(expression = "authentication") Authentication authentication,
+            Model model) {
+
+        try {
+            Customer savedCustomer = customerService.saveCustomer(user.getCustomer());
+            user.setCustomer(savedCustomer);
+            User savedUser = userService.saveUser(user);
+            model.addAttribute("savedUser", savedUser);
+        }  catch (IllegalStateException e) {
+            model.addAttribute("error", e.getMessage());
+            return "error";
+        }
+
+        // redirect based on authenticated role
+        User principal = (User) authentication.getPrincipal();
+        if (principal != null) {
+            List<Authority> auth = ((User) principal).getAuthorities();
+            if (auth.get(0).getAuthority().equals(Authority.RoleEnum.ADMIN_ROLE.toString())) {
+                return "redirect:/admin-dashboard";
+            } else if (auth.get(0).getAuthority().equals(Authority.RoleEnum.USER_ROLE.toString())) {
+                // assumes USER_ROLE as ony have 2 roles in our system at the moment
+                return "redirect:/user-dashboard";
+            }
+        }
+
         return "index";
     }
+
+
 
 
 
