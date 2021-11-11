cb6169270436557eaa7c107def05d84392f377c5
sadat.malik <sadat.malik@me.com>
2021-11-11 11:13:30 +0000

    Cleaning up unused service properties in LoginController

--- src/main/java/com/sadatmalik/customerwebsite/controllers/LoginController.java
+++ src/main/java/com/sadatmalik/customerwebsite/controllers/LoginController.java
@@ -1,7 +1,5 @@
 package com.sadatmalik.customerwebsite.controllers;
 
-import com.sadatmalik.customerwebsite.services.CarService;
-import com.sadatmalik.customerwebsite.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class LoginController {
-
-    private final CustomerService customerService;
-    private final CarService carService;
 
     @GetMapping("/")
     public String loginPage() {
