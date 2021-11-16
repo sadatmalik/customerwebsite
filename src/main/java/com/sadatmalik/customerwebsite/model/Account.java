f456393bc53aa958fc9f3b146af4f5d6293007fb
sadat.malik <sadat.malik@me.com>
2021-11-16 09:23:19 +0000

    Generating some sample CSV data and related model objects for batch processing. Closes #38  Open

---
+++ src/main/java/com/sadatmalik/customerwebsite/model/Account.java
@@ -0,0 +1,28 @@
+package com.sadatmalik.customerwebsite.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Account {
+    @Id
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private Long creditCardNum;
+    private String creditCardType;
+    private String balance;
+    private Date updatedAt;
+}
